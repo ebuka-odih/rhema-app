7c65794e2edf3cb1e19c7cc89b3ec352a1066f5f
Your Name <you@example.com>
2026-01-31 17:16:21 +0100

    commit changes

--- backend/database/migrations/2026_01_31_152236_create_bible_bookmarks_table.php
+++ backend/database/migrations/2026_01_31_152236_create_bible_bookmarks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('bible_bookmarks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
             $table->string('version_id');
             $table->string('book');
             $table->integer('chapter');
