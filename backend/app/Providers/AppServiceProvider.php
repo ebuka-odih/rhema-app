9deaac8c096c891baa7be23086489c6acdea701a
Your Name <you@example.com>
2026-02-01 17:26:56 +0100

    fix(auth): globally define password reset URL generator to prevent route not defined error

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Schema;
 
 use Laravel\Sanctum\Sanctum;
 use App\Models\PersonalAccessToken;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+
+        // Define how the password reset URL should be generated for the mobile app/API
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return config('app.url') . '/password/reset/' . $token . '?email=' . urlencode($user->email);
+        });
     }
 }
